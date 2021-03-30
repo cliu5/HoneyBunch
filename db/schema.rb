48e406c0c8948c6e00deaa0d4f94c00cd34a30e5
Thomas Lim <tommylim2@gmail.com>
2021-03-30 01:19:35 -0400

    yml changed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20210326053135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "name"
     t.string "image"
